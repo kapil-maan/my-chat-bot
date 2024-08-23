5f35631808a5488c213690cc6b2669496a38331d
Kapil Kumar <145493471+kapil-maan@users.noreply.github.com>
2024-08-23 08:20:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let userMessage = null; // Variable to store user's message
 const inputInitHeight = chatInput.scrollHeight;
 
 // API configuration
-const API_KEY = "AIzaSyBNIW-XvlNnx36MMUmtv1prGtlSkXdcnMs"; // Your API key here
+const API_KEY = ""; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 const createChatLi = (message, className) => {
   // Create a chat <li> element with passed message and className
